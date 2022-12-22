6484ec69165eda878a75d73663b93e6536d90a3b
Seokmin <newacon@naver.com>
2022-12-22 22:23:39 +0200

    Clone from 2

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  accentColor: "#4cd137",
+  cardBgColor: "transparent",
+};
